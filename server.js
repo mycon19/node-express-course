24f00fc4069aad29d7e5e4bd9e4cf51b4519c0bb
robomocha <govi@alphaori.sg>
2020-08-03 23:07:37 -0700

    added few more API end points

--- server.js
+++ server.js
@@ -3,10 +3,19 @@
 const express = require('express');
 const app = express();
 
+// DB Data - mock up here ... 
+
 const mockUserData=[
 	{name:'Govi'},
 	{name:'Sakthi'}
 ]
+const mockRoleData=[
+	{name:'admin'},
+	{name:'program manager'},
+	{name:'CTO'},
+	{name:'product manager'}
+]
+
 
 app.get('/users',function(req,res){
 	res.json({
@@ -15,7 +24,13 @@ app.get('/users',function(req,res){
 		users: mockUserData
 	})
 })
-
+app.get('/roles',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got Roles. Nice!',
+		users: mockRoleData
+	})
+})
 
 // The app.listen method will start up the server locally on the port you give as its first argument 
 // (in this case the base url is: http://localhost:8000)
